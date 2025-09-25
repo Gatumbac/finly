513ed42f94965d9aa50f78e150c6bbe98f693503
Gatumbac <gatumbac@espol.edu.ec>
2025-09-25 12:22:13 -0500

    Project Setup with Node.js, Express and Tailwind

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Index',
+        message: 'Hello from Node.js!'
+    })
+});
+
+app.get('/contact', (req, res) => {
+    res.render('index', {
+        title: 'Contact',
+        message: 'The Contact Page!'
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('index', {
+        title: 'About',
+        message: 'The About Page!'
+    })
+});
+
+app.use((req, res) => {
+    res.status(404).render('index', {
+        title: 'Not Found',
+        message: 'Page Not Found!'
+    })
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
